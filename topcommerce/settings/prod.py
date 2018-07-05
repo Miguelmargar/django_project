ed686b24014d45b5c6edcede7f5c38ceef7852cb
Miguel Martinez <miguelitalia@hotmail.com>
2018-07-05 16:00:11 +0000

    amended prod to add staticfiles and added heroku extra config vars to heroku to deploy

--- topcommerce/settings/prod.py
+++ topcommerce/settings/prod.py
@@ -11,3 +11,10 @@ ALLOWED_HOSTS = ["django-project-miguelmargar.c9users.io"]
 DATABASES = {
    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
