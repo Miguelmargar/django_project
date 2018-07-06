4224efc81cc7a67ed0165efc98165fb7c7a83859
Miguel Martinez <miguelitalia@hotmail.com>
2018-07-06 11:08:31 +0000

    added products app and created one to test

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.db.models import Avg
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=25, default='')
+    description = models.TextField()
+    location = models.CharField(max_length=25, default='')
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    turnover = models.DecimalField(max_digits=15, decimal_places=2, default='0')
+    net_profit = models.DecimalField(max_digits=15, decimal_places=2, default='0')
+    image = models.ImageField(upload_to='images')
+    brand = models.CharField(max_length=50, default='')
+    
+    def __str__(self):
+        return self.name
--- topcommerce/urls.py
+++ topcommerce/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import get_products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_products, name="get_products")
 ]
